6c2cdd3a897d497d4dfbb7969feecb8ca8bf09c8
Artyom <167999696+Scorp355@users.noreply.github.com>
2026-06-30 10:48:02 +0500

    Реализована базовая размета и наследование в hotels и rooms

--- hotels/models.py
+++ hotels/models.py
@@ -8,7 +8,7 @@ class Hotel(models.Model):
     address = models.CharField(max_length=255)
 
     # Цена за 1 ноч проживания
-    price = models.DecimalField(max_digits=10, decimal_places=2)
+    price_night = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.FloatField(default=0)
     is_active = models.BooleanField(default=True)
     created_at = models.DateTimeField(auto_now_add=True)
