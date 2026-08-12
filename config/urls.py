4b5280f6cd41950f5861b1c037b31b50b83759b3
Artyom <scorpy_rock@mail.ru>
2026-08-12 10:38:53 +0500

    В рамках ДЗ доработано приложение users

--- config/urls.py
+++ config/urls.py
@@ -7,7 +7,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('core.urls')),
+    # path('', include('core.urls')),
     path('users/', include('users.urls')),
     path('hotels/', include('hotels.urls')),
     path('rooms/', include('rooms.urls')),
