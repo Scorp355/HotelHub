7b7c9555e72e144ac17511ae54aa08c7a741e061
Artyom <scorpy_rock@mail.ru>
2026-08-27 21:45:13 +0500

    ачали views.py в bookings

--- bookings/views.py
+++ bookings/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from django.core.exceptions import ValidationError
 
-# Create your views here.
+from rooms.models import Room
+from bookings.models import Booking
+from .forms import BookingForm
+from . import services
+
