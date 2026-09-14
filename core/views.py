e07ff2e1df4dd338697ded5c93f5ee29a88c3ac3
Artyom <scorpy_rock@mail.ru>
2026-09-14 21:58:23 +0500

    Настроили core, начали бизнес-логику core

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from hotels.models import Hotel
+from rooms.models import Room
 
-# Create your views here.
+# Главная функция нашей страницы
+def home(request):
+    top_hotels = Hotel.objects.filter(is_active=True).order_by('-rating')[:3]
+    feautured_rooms = Room.objects.filter(is_available=True).select_related('hotel')[:3]
+
+    stats = {
+            'hotels': Hotel.objects.filter(is_active=True).count(),
+            'rooms': Room.objects.filter(is_available=True).count(),
+            'cities': Hotel.objects.values('city').distinct().count()
+        }
